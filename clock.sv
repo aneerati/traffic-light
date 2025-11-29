3a30bfec96e15471fd4bd6e7476c4e4ae08e728a
c-scheckel <scheckel@purdue.edu>
2025-11-29 16:25:16 -0500

    Simplify enable condition in clock.sv
    Refactor enable logic for clock based on counter values.

--- clock.sv
+++ clock.sv
@@ -9,16 +9,7 @@ module clock
 
 always_comb begin
     enable = 1'b0;
-    if(PED) begin
-        enable =  (counter == 6'd11 ||
-                  counter == 6'd13 ||
-                  counter == 6'd15 ||
-                  counter == 6'd21 ||
-                  counter == 6'd23 ||
-                  counter == 6'd33);
-    end 
-
-    else begin
+    
         enable = (counter == 6'd15 ||
                   counter == 6'd17 ||
                   counter == 6'd19 ||
