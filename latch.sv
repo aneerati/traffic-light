cc84dcbf324c5ef8bd73e916b74acf3fddf40ce1
c-scheckel <scheckel@purdue.edu>
2025-11-29 14:05:06 -0500

    Implement behavioral SR latch in latch.sv

---
+++ latch.sv
@@ -0,0 +1,16 @@
+`default_nettype none
+module SRLatch_Behavioral (
+	input  logic S,
+	input  logic R,
+	output logic Q,
+	output logic Qn
+	);
+	always_comb 
+		case ({S, R})
+			2'b00: Q = Q;
+			2'b01: Q = 0;
+			2'b10: Q = 1;
+			2'b11: Q = 1'bx;
+		endcase
+	assign Qn = ~Q;
+endmodule
