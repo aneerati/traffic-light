87dc25a614b798fbb7aabf7e6879c5c9b7378321
c-scheckel <scheckel@purdue.edu>
2025-11-30 01:03:41 -0500

    Add trafficTop module with inputs and outputs

--- trafficTop.sv
+++ trafficTop.sv
@@ -1,1 +1,24 @@
+module trafficTop(
+  input logic clk,
+  input logic reset,
+  input logic pedButton,
+  input logic [2:0] mainTrafficIn,
+  input logic [2:0] sideTrafficIn,
 
+  output logic MG,
+  output logic MY,
+  output logic MR,
+  output logic SG,
+  output logic SY,
+  output logic SR,
+  output logic pedLight
+);
+
+  logic [5:0] counter;
+  
+  sixbitcounter trafficCounter (.clk(clk),
+                                .reset(reset),
+                                .Q(counter));
+
+  
+  
