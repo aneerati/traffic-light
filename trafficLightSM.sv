b4ce3f9a24fef787a057b3e29dd8ab975c568e88
c-scheckel <scheckel@purdue.edu>
2025-11-30 19:53:24 -0500

    Refactor traffic light state machine logic

--- trafficLightSM.sv
+++ trafficLightSM.sv
@@ -1,6 +1,5 @@
 `default_nettype none
 module trafficLightSM (
-    
     input logic pedButton,
     input logic en,
     input logic reset,
@@ -14,8 +13,6 @@ module trafficLightSM (
     output logic pedLight,
     output logic newCycle
 );
-
-    //Main Street, Side Street
     typedef enum bit[2:0] {
         GR = 3'b000,
         YR = 3'b001,
@@ -25,21 +22,29 @@ module trafficLightSM (
         RR2 = 3'b101,
         PED = 3'b110
     } state;
-    
     state Q = GR, nextQ;
+    logic pedReset;
+    logic pedOnInt;
     
-    logic pedReset = 1'b0;
-
-    latch pedLatch (.S(pedButton & ~pedReset), .R(pedReset),
-                    .Q(pedOn));
-    
+    always_ff @(posedge en or posedge reset) begin
+        if (reset)
+            pedOnInt <= 1'b0;
+        else if (pedReset)
+            pedOnInt <= 1'b0;
+        else if (pedButton)
+            pedOnInt <= 1'b1;
+        else
+            pedOnInt <= pedOnInt;
+    end
+      
+    assign pedOn = pedOnInt;    
+        
     always_ff @(posedge en or posedge reset) begin
         if (reset)
             Q <= GR;
         else 
             Q <= nextQ;
     end
-            
     always_comb begin
         case (Q)
             GR: nextQ = YR;
@@ -47,11 +52,11 @@ module trafficLightSM (
             RR1: nextQ = RG;
             RG: nextQ = RY;
             RY: nextQ = RR2;
-            RR2: nextQ = state'((pedOn == 1'b1) ? PED : GR);
+            RR2: nextQ = state'((pedOnInt == 1'b1) ? PED : GR);
             PED: nextQ = GR; 
+            default: nextQ = GR;
         endcase
     end
-    
     always_comb begin
         MG = 1'b0;
         MY = 1'b0;
@@ -62,39 +67,28 @@ module trafficLightSM (
         pedLight = 1'b0;
         pedReset = 1'b0;
         newCycle = 1'b0;
-        
         case (Q)
             GR: begin
                 MG = 1'b1;
                 SR = 1'b1;
-                pedReset = 1'b0;
                 newCycle = 1'b1;
             end
-            
             YR: begin
                 MY = 1'b1;
                 SR = 1'b1;
-                pedReset = 1'b0;
             end
-            
             RR1, RR2: begin
                 MR = 1'b1;
                 SR = 1'b1;
-                pedReset = 1'b0;
             end
-            
             RG: begin
                 MR = 1'b1;
                 SG = 1'b1;
-                pedReset = 1'b0;
             end
-            
             RY: begin
                 MR = 1'b1;
                 SY = 1'b1;
-                pedReset = 1'b0;
             end
-            
             PED: begin
                 MR = 1'b1;
                 SR = 1'b1;
@@ -103,6 +97,4 @@ module trafficLightSM (
             end
         endcase
     end
-    
-    
 endmodule
